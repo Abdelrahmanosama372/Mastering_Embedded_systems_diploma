ad22db733c2a28caa3b2de52b008d04329909c4c
abdelrahman <abdelrahmanosama372@gmail.com>
2024-08-16 15:11:31 +0300

    lesson 4

---
+++ Assignments/unit_14_Mastering_ARM_Cortex/lesson 4/README.md
@@ -0,0 +1,12 @@
+# LAB topic
+the lab is about switching form privileged to unprivileged mode and vice versa
+
+# LAB simulation in keil
+- before switching to unprivileged mode 
+![Alt text](./before_switching.png)
+
+- after switching to unprivileged mode 
+![Alt text](./after_switching.png)
+
+- switching back to privileged mode in ISR
+![Alt text](./switching_back_to_privillaged.png)
---
+++ Assignments/unit_14_Mastering_ARM_Cortex/lesson 4/drivers_new/Debug/stm32f103x6_drivers/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../stm32f103x6_drivers/stm32f103x6_EXTI_driver.c \
+../stm32f103x6_drivers/stm32f103x6_gpio_driver.c 
+
+OBJS += \
+./stm32f103x6_drivers/stm32f103x6_EXTI_driver.o \
+./stm32f103x6_drivers/stm32f103x6_gpio_driver.o 
+
+C_DEPS += \
+./stm32f103x6_drivers/stm32f103x6_EXTI_driver.d \
+./stm32f103x6_drivers/stm32f103x6_gpio_driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+stm32f103x6_drivers/%.o stm32f103x6_drivers/%.su stm32f103x6_drivers/%.cyclo: ../stm32f103x6_drivers/%.c stm32f103x6_drivers/subdir.mk
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I"E:/stm/drivers_new/stm32f103x6_drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-stm32f103x6_drivers
+
+clean-stm32f103x6_drivers:
+	-$(RM) ./stm32f103x6_drivers/stm32f103x6_EXTI_driver.cyclo ./stm32f103x6_drivers/stm32f103x6_EXTI_driver.d ./stm32f103x6_drivers/stm32f103x6_EXTI_driver.o ./stm32f103x6_drivers/stm32f103x6_EXTI_driver.su ./stm32f103x6_drivers/stm32f103x6_gpio_driver.cyclo ./stm32f103x6_drivers/stm32f103x6_gpio_driver.d ./stm32f103x6_drivers/stm32f103x6_gpio_driver.o ./stm32f103x6_drivers/stm32f103x6_gpio_driver.su
+
+.PHONY: clean-stm32f103x6_drivers
+
